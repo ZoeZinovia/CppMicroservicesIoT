99ed4daa145b44402c6ba13785ce2d5531d84dfc
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-20 14:19:48 +0100

    Adding CPP led blinker code to git

--- ledMicroserviceSubscriber.cpp
+++ ledMicroserviceSubscriber.cpp
@@ -1,12 +1,42 @@
+//
+// Created by Shani du Plessis on 20/04/2021.
+//
+
+extern "C" {
 #include <wiringPi.h>
-int main (void)
-{
-    wiringPiSetup () ;
-    pinMode (4, OUTPUT) ;
-    for (;;)
-    {
-        digitalWrite (4, HIGH) ; delay (500) ;
-        digitalWrite (4,  LOW) ; delay (500) ;
-    }
-    return 0 ;
+}
+#include <csignal>
+#include <iostream>
+using namespace std;
+
+bool RUNNING = true;
+
+void blink_led(int pin, int time){
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+    delay(time);
+    digitalWrite(pin, LOW);
+    delay(time);
+}
+
+void exit_program(int s){
+    RUNNING = false;
+}
+
+int main(){
+//    std::signal(SIGINT, exit_program);
+
+    wiringPiSetupGpio();
+
+//    std::cout << "Controlling the GPIO pins with wiringPi\n";
+
+    int pin = 4;
+
+    int time = 1000;
+//    while(RUNNING){
+    blink_led(pin, time);
+//    }
+
+//    std::cout << "Program ended";
+
 }
