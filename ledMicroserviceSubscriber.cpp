72929dc3d0c2cb4534eb483439d86cf2a0db8f6f
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-20 14:27:44 +0100

    Adding CPP led blinker code to git

--- ledMicroserviceSubscriber.cpp
+++ ledMicroserviceSubscriber.cpp
@@ -2,41 +2,41 @@
 // Created by Shani du Plessis on 20/04/2021.
 //
 
-extern "C" {
-#include <wiringPi.h>
-}
+//extern "C" {
+//#include <wiringPi.h>
+//}
 #include <csignal>
-//#include <iostream>
+#include <iostream>
 //using namespace std;
 
-bool RUNNING = true;
-
-void blink_led(int pin, int time){
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, HIGH);
-    delay(time);
-    digitalWrite(pin, LOW);
-    delay(time);
-}
-
-void exit_program(int s){
-    RUNNING = false;
-}
+//bool RUNNING = true;
+//
+//void blink_led(int pin, int time){
+//    pinMode(pin, OUTPUT);
+//    digitalWrite(pin, HIGH);
+//    delay(time);
+//    digitalWrite(pin, LOW);
+//    delay(time);
+//}
+//
+//void exit_program(int s){
+//    RUNNING = false;
+//}
 
 int main(){
 //    std::signal(SIGINT, exit_program);
 
-    wiringPiSetupGpio();
+//    wiringPiSetupGpio();
 
-//    std::cout << "Controlling the GPIO pins with wiringPi\n";
+    std::cout << "Controlling the GPIO pins with wiringPi\n";
 
-    int pin = 4;
-
-    int time = 1000;
-//    while(RUNNING){
-    blink_led(pin, time);
-//    }
-
-//    std::cout << "Program ended";
+//    int pin = 4;
+//
+//    int time = 1000;
+////    while(RUNNING){
+//    blink_led(pin, time);
+////    }
+//
+////    std::cout << "Program ended";
 
 }
