54befe780a2895c98c8a09c302408484d6ab1f28
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-23 12:29:17 +0100

    Debugging "Done" message

--- ledMicroserviceSubscriber.cpp
+++ ledMicroserviceSubscriber.cpp
@@ -58,9 +58,9 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
     char* payloadptr;
 
     printf("Message arrived\n");
-    std::cout << message << "\n";
     payloadptr = (char*)message->payload;
     int len = strlen(payloadptr);
+    std::cout << payloadptr[len-1];
     if(payloadptr[len-1] == payloadptr[len-2] == '}'){
         payloadptr[len-1] = '\0';
     }
