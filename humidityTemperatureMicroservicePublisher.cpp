33355c6b284a97790f649fa40e431a948b8a26c3
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-26 15:41:40 +0100

    Debugging publisher file

--- humidityTemperatureMicroservicePublisher.cpp
+++ humidityTemperatureMicroservicePublisher.cpp
@@ -181,16 +181,26 @@
  *******************************************************************************/
 
 extern "C" {
+    #include <wiringPi.h>
     #include <stdio.h>
     #include <stdlib.h>
     #include <string.h>
     #include "MQTTClient.h"
 }
+#include <csignal>
+#include <iostream>
+#include "include/rapidjson/document.h"
+#include "include/rapidjson/stringbuffer.h"
+#include "include/rapidjson/prettywriter.h"
+#include "include/rapidjson/writer.h"
+#include <chrono>
+#include <fstream>
+#include <typeinfo>
 
 #define ADDRESS     "10.35.0.229:1883"
 #define CLIENTID    "ExampleClientPub"
 #define TOPIC       "MQTT"
-char* PAYLOAD =      "Hello World!";
+std::string PAYLOAD =      "Hello World!";
 #define QOS         1
 #define TIMEOUT     10000L
 
@@ -212,14 +222,15 @@ int main(int argc, char* argv[])
         printf("Failed to connect, return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
+
     pubmsg.payload = PAYLOAD;
-    pubmsg.payloadlen = (int)strlen(PAYLOAD);
+//    pubmsg.payloadlen = (int)std::strlen(PAYLOAD);
     pubmsg.qos = QOS;
     pubmsg.retained = 0;
     MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-    printf("Waiting for up to %d seconds for publication of %s\n"
-           "on topic %s for client with ClientID: %s\n",
-           (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
+//    printf("Waiting for up to %d seconds for publication of %s\n"
+//           "on topic %s for client with ClientID: %s\n",
+//           (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
     printf("Message with delivery token %d delivered\n", token);
     MQTTClient_disconnect(client, 10000);
